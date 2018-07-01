219e4c0d724bdf8a1e683d80d9a92e988f4001e6
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-07-02 01:09:37 +0530

    Upgraded to Latest version
    integrated eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
-   devtool: "inline-source-map",
-   entry: [
-      "webpack-dev-server/client?http://127.0.0.1:8080/",
-      "webpack/hot/only-dev-server",
-      "./src"
-   ],
-   output: {
-      path: path.join(__dirname, 'public'),
-      filename: 'bundle.js'
-   },
-   module: {
-      loaders:[
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loaders: ['react-hot-loader','babel-loader?presets[]=react,presets[]=es2015']
-         }
-      ]
-   },
-   plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoEmitOnErrorsPlugin()
-   ]
-}
+  devtool: "inline-source-map",
+  entry: ["./src"],
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, "./src")],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["react", "es2015"],
+              cacheDirectory: true,
+              plugins: ["react-hot-loader/babel"]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  mode: "development"
+};
